60ff51dc4b1efefd472c7e5e9631265a0454c2d4
Andrew Ransom <andrew@bookqube.com>
2025-06-04 15:26:50 -0700

    x

--- app/customer-upload/[token]/page.tsx
+++ app/customer-upload/[token]/page.tsx
@@ -141,6 +141,7 @@ export default function CustomerUploadPage() {
   }
 
   return (
+    <div className='fixed top-0 left-0 right-0 bottom-0 z-50'>
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-2xl mx-auto p-4">
         {/* Header */}
@@ -223,6 +224,7 @@ export default function CustomerUploadPage() {
           </div>
         )}
       </div>
+    </div>
     </div>
   );
 }
